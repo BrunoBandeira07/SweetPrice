a386b859b3ae26fed8246634d508166b5a8e69f4
Bruno Gomes Bandeira <mkt.brunoband@gmail.com>
2025-09-24 13:14:29 +0000

    Try fixing this error: `Runtime Error: Error: (0 , {imported module [pro

--- src/components/app/substitution-finder.tsx
+++ src/components/app/substitution-finder.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { useActionState, useFormStatus } from 'react-dom';
+import { useActionState } from 'react';
+import { useFormStatus } from 'react-dom';
 import { HelpCircle, Loader2 } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import {
