72bd30cd552c6ab2c49de4a15c03b55cae9be069
Bruno Gomes Bandeira <mkt.brunoband@gmail.com>
2025-10-11 17:39:28 +0000

    fix: login

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-8959486326-c6cd9",
-  "appId": "1:519327508007:web:46a76b81a4eadfefbf1002",
-  "apiKey": "AIzaSyCUEqP3--W-PCWtUZU2zlh1K029HyLLQcM",
-  "authDomain": "studio-8959486326-c6cd9.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "519327508007"
+  apiKey: "AIzaSyCUEqP3--W-PCWtUZU2zlh1K029HyLLQcM",
+  authDomain: "studio-8959486326-c6cd9.firebaseapp.com",
+  projectId: "studio-8959486326-c6cd9",
+  storageBucket: "studio-8959486326-c6cd9.firebasestorage.app",
+  messagingSenderId: "519327508007",
+  appId: "1:519327508007:web:46a76b81a4eadfefbf1002"
 };
